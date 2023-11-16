00a3bc0430756e1b0df713f1944a24aca5a5105e
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-16 16:00:22 +0100

    Check that it works

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var exec = require('child_process').exec;
 
 app.get('/', (req, res) => {
   exec("dfx canister --network http://localhost:8080 call qsgjb-riaaa-aaaaa-aaaga-cai get_stats", (err, stdout) => {
-    res.send(stdout)
+    console.log(stdout);
+    res.send("hello world");
   })
 })
 
