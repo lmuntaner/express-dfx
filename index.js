1a6127350069732f4e7a03cc6d075c214a1998e0
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-16 16:01:42 +0100

    Port 3000

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
 var exec = require('child_process').exec;
 
 app.get('/', (req, res) => {
-  exec("dfx canister --network http://localhost:8080 call qsgjb-riaaa-aaaaa-aaaga-cai get_stats", (err, stdout) => {
+  exec("dfx canister --network http://localhost:8080 call qsgjb-riaaa-aaaaa-aaaga-cai get_stats", (err, stdout, stderr) => {
     console.log(stdout);
+    console.log(stderr)
     res.send("hello world");
   })
 })
